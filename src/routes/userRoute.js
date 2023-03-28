caade98a3f4a2f6c7a725d93c8744c73de9f170c
Huu Tu <huutu7165@gmail.com>
2023-03-28 15:17:34 +0700

    Ghi chú vdsad

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -16,6 +16,7 @@ router.post('/api/v1/register', register);
 router.post('/api/v1/dsad', register);
 router.post('/api/v1/dđ', register);
 router.post('/api/v1/login', login);
+router.post('/api/v1/dsads', login);
 router.put('/api/v1/update', [AuthJwt.checkLogin], updateAccout);
 router.get('/api/v1/token', [AuthJwt.checkLogin], token);
 router.get('/api/v1/logout', logout);
